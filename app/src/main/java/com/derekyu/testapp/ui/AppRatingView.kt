91bb4c58e43337a033cbbb26350d0b11a3e92fea
yychun <yuyauchun@gmail.com>
2020-08-14 14:31:17 +0800

    fix wrong rating after scrolling back to previously bound items

--- app/src/main/java/com/derekyu/testapp/ui/AppRatingView.kt
+++ app/src/main/java/com/derekyu/testapp/ui/AppRatingView.kt
@@ -23,20 +23,31 @@ class AppRatingView @JvmOverloads constructor(
     fun setRating(rating: Float, ratingCount: Int) {
         val ratingInt = rating.roundToInt()
         val starOn = ContextCompat.getDrawable(context, android.R.drawable.star_on)
+        val starOff = ContextCompat.getDrawable(context, android.R.drawable.star_off)
         if (ratingInt > 0) {
             star1.setImageDrawable(starOn)
+        } else {
+            star1.setImageDrawable(starOff)
         }
         if (ratingInt > 1) {
             star2.setImageDrawable(starOn)
+        } else {
+            star2.setImageDrawable(starOff)
         }
         if (ratingInt > 2) {
             star3.setImageDrawable(starOn)
+        } else {
+            star3.setImageDrawable(starOff)
         }
         if (ratingInt > 3) {
             star4.setImageDrawable(starOn)
+        } else {
+            star4.setImageDrawable(starOff)
         }
         if (ratingInt > 4) {
             star5.setImageDrawable(starOn)
+        } else {
+            star5.setImageDrawable(starOff)
         }
         rating_count.text = "(${ratingCount.toString()})"
     }
